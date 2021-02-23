fc9010338b10f370a03ba8f4c0e9fb608cbb1e48
Jarquevious <jarquevious@gmail.com>
2021-02-23 14:08:04 -0800

    first commit

---
+++ test_two.py
@@ -0,0 +1,13 @@
+def get_average(li):
+    sum = 0
+    for num in li:
+        sum += num
+    mean = sum / len(li)
+    return mean
+
+def get_average():
+    sum=0
+    for num in li:
+        sum+=num
+    mean = sum/len(li)
+    return 
